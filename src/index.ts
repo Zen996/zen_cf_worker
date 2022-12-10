244a81686fa6efbeef857dc7ee144651aa877293
Zen <zengan1996@gmail.com>
2022-12-11 00:37:26 +0800

    Initial commit for cf worker

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `wrangler dev src/index.ts` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export interface Env {
+	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+	// MY_KV_NAMESPACE: KVNamespace;
+	//
+	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+	// MY_DURABLE_OBJECT: DurableObjectNamespace;
+	//
+	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+	// MY_BUCKET: R2Bucket;
+}
+
+export default {
+	async fetch(
+		request: Request,
+		env: Env,
+		ctx: ExecutionContext
+	): Promise<Response> {
+		return await handleRequest(request);
+	},
+};
+
+async function handleRequest(request) {
+  const clientIP = request.headers.get('CF-Connecting-IP');
+
+  if (request.cf ) {
+    var clientASN = request.cf.asn
+    var clientCountry = request.cf.country
+  }
+  if (clientCountry != null && clientCountry != "SG") {
+    return Response.redirect("https://1.1.1.1/");
+  } 
+  return new Response("This is your IP " + clientIP + " and you are accessing this site from "+ clientCountry + "|" + clientASN)
+}
